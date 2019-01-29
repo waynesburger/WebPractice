f33fb44bd053e6560c9a286dd54d9780fda54bb8
Wayne Weidert <freeholays@hotmail.com>
2019-01-29 00:32:41 -0800

    Add files via upload

---
+++ MorseCode/src/php/feedback.php
@@ -0,0 +1,16 @@
+<?php
+$first_name = $_POST['firstname'];
+$last_name  = $_POST['lastname'];
+$email_addr = $_POST['emailaddr'];
+$phone_num  = $_POST['phonenumb'];
+$contact_method = $_POST['howtocontact'];
+$raw_message = $_POST['message'];
+
+$full_message = "$raw_message \n\n$first_name $last_name\nphone: $phone_num";
+$recipient    = "waynewdata@outlook.com";
+$subject      = "Feedback";
+$mailheader   = "From: $email_addr \r\n";
+
+mail($recipient, $subject, $full_message, $mailheader) or die("Error");
+echo "Thanks for your input! We'll get back to you soon."
+?>
